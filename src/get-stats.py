9a0bf55d088bea5e78ebee33ce4ce7e1a5251c4c
Nícolas Leão <nicolas-armando@hotmail.com>
2026-04-01 01:01:08 -0300

    feat: implement get_stats

---
+++ src/get-stats.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+def get_stats(vocab):
+    pairs = defaultdict(int)
+    for word, freq in vocab.items():
+        symbols = word.split()
+        for i in range(len(symbols) - 1):
+            pairs[(symbols[i], symbols[i + 1])] += freq
+    return pairs
